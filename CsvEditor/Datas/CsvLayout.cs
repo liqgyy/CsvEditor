72d787e8612eac01ec8cb067ddaa888f72e851f8
huangwwmm <hwm@huangwm.com>
2017-12-26 15:46:41 +0800

    移除 CsvLayout过时属性

--- CsvEditor/Datas/CsvLayout.cs
+++ CsvEditor/Datas/CsvLayout.cs
@@ -5,12 +5,6 @@ public class CsvLayout
 {
 	public string Path;
 	public int[] ColumnWidths;
-	public int[] RowHeights;
-	/// <summary>
-	/// 第一列(行号)的宽度
-	/// 小于0时不设置
-	/// </summary>
-	public int RowHeadersWidth = -1;
 
 	/// <summary>
 	/// 小于0时不冻结
